610b7e07be3986fab43b1612fad6e5dfceb77783
dbc2201 <divyanshb22@gmail.com>
2022-01-31 21:01:58 +0530

    Card wali class ke liye equals() and hashCode() banaya hai

--- src/main/java/card/Card.java
+++ src/main/java/card/Card.java
@@ -1,5 +1,7 @@
 package card;
 
+import java.util.Objects;
+
 public class Card {
     private String name;
     private int value;
@@ -23,6 +25,19 @@ public class Card {
 
     public void setValue(int value) {
         this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return value == card.value && Objects.equals(name, card.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, value);
     }
 
     @Override
